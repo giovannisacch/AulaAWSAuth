8c1abbd0976d174a10ffab9393513d93909aa3ec
Luiz Pachioni <lopachioni@gmail.com>
2022-07-07 20:33:28 -0300

    criando api para deletar imagens do s3

--- AulaAWS.Web/Controllers/ImagesController.cs
+++ AulaAWS.Web/Controllers/ImagesController.cs
@@ -1,4 +1,5 @@
 using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AulaAWS.Web.Controllers
@@ -8,6 +9,7 @@ namespace AulaAWS.Web.Controllers
     public class ImagesController : ControllerBase
     {
         private readonly IAmazonS3 _amazonS3;
+        private readonly List<string> _imageFormats = new List<string>() { "image/jpeg", "image/png" };
 
         public ImagesController(IAmazonS3 amazonS3)
         {
@@ -15,9 +17,27 @@ namespace AulaAWS.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateImage(string nameBucket)
+        public async Task<IActionResult> CreateImage(IFormFile image)
         {
-            var response = await _amazonS3.PutBucketAsync(nameBucket);
+            if (!_imageFormats.Contains(image.ContentType))
+                return BadRequest("Tipo Inválido");
+            using (var imageStream = new MemoryStream())
+            {
+                image.CopyToAsync(imageStream);
+
+                var request = new PutObjectRequest();
+                request.Key = "reconhecimento" + image.FileName;
+                request.BucketName = "aula-imagens";
+                request.InputStream = imageStream;
+
+                var response = await _amazonS3.PutObjectAsync(request);
+                return Ok(response);
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteImage(string nameImageInS3)
+        {
+            var response = await _amazonS3.DeleteObjectAsync("aula-imagens", nameImageInS3);
             return Ok(response);
         }
         [HttpPost("bucket")]
@@ -26,7 +46,7 @@ namespace AulaAWS.Web.Controllers
             var response = await _amazonS3.PutBucketAsync(nameBucket);
             return Ok(response);
         }
-         [HttpGet("bucket")]
+        [HttpGet("bucket")]
         public async Task<IActionResult> ReadBucket()
         {
             var response = await _amazonS3.ListBucketsAsync();
