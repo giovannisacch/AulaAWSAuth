63c5bc94cd4ddf0f7f0b8ba76d41183777ad9640
Luiz Pachioni <lopachioni@gmail.com>
2022-07-13 23:04:05 -0300

    criando api para analisar e comparar a semelhança de um rosto em duas imagens diferentes

--- AulaAWS.Web/Controllers/RekognitionController.cs
+++ AulaAWS.Web/Controllers/RekognitionController.cs
@@ -13,11 +13,35 @@ namespace AulaAWS.Web.Controllers
         {
             _rekognitionClient = rekognitionClient;
         }
+        [HttpPost]
+        public async Task<IActionResult> CompararRosto(string nameImageInS3, IFormFile imageLogin)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await imageLogin.CopyToAsync(memoryStream);
+                var targetImage = new Image();
+                targetImage.Bytes = memoryStream;
+
+                var request = new CompareFacesRequest();
+                var sourceImage = new Image();
+                var s3ObjectSource = new S3Object();
+
+                s3ObjectSource.Bucket = "aula-imagens";
+                s3ObjectSource.Name = nameImageInS3;
+
+                sourceImage.S3Object = s3ObjectSource;
+                request.TargetImage = targetImage;
+                request.SourceImage = sourceImage;
+
+                var response = await _rekognitionClient.CompareFacesAsync(request);
+                return Ok(response);
+            }
+        }
         [HttpGet]
         public async Task<IActionResult> AnalisarRosto(string nameImage)
         {
             var request = new DetectFacesRequest();
-            request.Attributes = new List<string>() { "ALL" };
+
             var image = new Image();
             var s3Object = new S3Object();
 
@@ -26,6 +50,7 @@ namespace AulaAWS.Web.Controllers
 
             image.S3Object = s3Object;
             request.Image = image;
+            request.Attributes = new List<string>() { "ALL" };
 
 
             var response = await _rekognitionClient.DetectFacesAsync(request);
@@ -33,8 +58,8 @@ namespace AulaAWS.Web.Controllers
             var listaRostos = response.FaceDetails;
 
             var rostoAtual = response.FaceDetails.First();
-            
-            if ((response.FaceDetails.Count() == 1) & (rostoAtual.Eyeglasses.Value == false))
+
+            if ((response.FaceDetails.Count == 1) || (rostoAtual.Eyeglasses.Value == false))
                 return Ok(response);
             else
                 return BadRequest("Essa imagem não contém um rosto ou possui um rosto com óculos ou possui mais de um rosto!");
